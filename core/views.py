1630943f376c87e8e2d04e8ba30ad578c816612e
Josh Grein <joshgrein2@gmail.com>
2025-03-13 21:56:21 -0400

    Make /profiles/ a forbidden 403 endpoint. This will change later.

--- core/views.py
+++ core/views.py
@@ -1,11 +1,16 @@
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProfileSerializer
 from .models import Profile
 
 # Create your views here.
 
 
-class ProfileViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class ProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_403_FORBIDDEN)
