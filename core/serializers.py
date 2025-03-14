4b4e4961c095eb97595eda6294b3ba769a8b0fae
Josh Grein <joshgrein2@gmail.com>
2025-03-13 20:56:38 -0400

    Fix Portfolio implementation.

--- core/serializers.py
+++ core/serializers.py
@@ -4,13 +4,13 @@ from portfolio.serializers import PortfolioSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    individual_portfolio = PortfolioSerializer(read_only=True)
+    portfolio = PortfolioSerializer(read_only=True)
     email = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
         fields = ['id', 'user', 'email', 'currency', 'language',
-                  'birth_date', 'is_asset_manager', 'individual_portfolio']
+                  'birth_date', 'is_asset_manager', 'portfolio']
         read_only_fields = ['user']
 
     def get_email(self, obj):
