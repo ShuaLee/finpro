be37a89f7f695cbaadb5217e9b2105e11f29ba87
Josh Grein <engineer@STANMECH.local>
2025-05-27 13:55:31 -0400

    Include StockPortfolio in PortfolioSerializer.

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
+from stock_portfolio.models import StockPortfolio
+from stock_portfolio.serializers import StockPortfolioSerializer
 from .models import Portfolio
 
 
 class PortfolioSerializer(serializers.ModelSerializer):
+    stock_portfolio = serializers.SerializerMethodField()
+
     class Meta:
         model = Portfolio
-        fields = ['id', 'created_at']
+        fields = ['created_at', 'stock_portfolio']
+
+    def get_stock_portfolio(self, obj):
+        try:
+            stock_portfolio = StockPortfolio.objects.get(portfolio=obj)
+            return {
+                'created_at': stock_portfolio.created_at,
+            }
+        except StockPortfolio.DoesNotExist:
+            return None
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from .views import PortfolioDetailView
 
 
 urlpatterns = [
     path('', PortfolioDetailView.as_view(), name='portfolio-detail'),
-    # path('stock-portfolio/', include('stock_portfolio.urls')),
 ]
