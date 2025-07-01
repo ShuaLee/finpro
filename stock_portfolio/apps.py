ae5930864f9c3f9e4ef47e4eaaca013b2e2625a9
Josh Grein <joshgrein2@gmail.com>
2025-07-01 12:50:16 -0400

    Removed StockPortfolio app.

--- stock_portfolio/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockPortfolioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stock_portfolio'
-
-    def ready(self):
-        import stock_portfolio.signals
